7c854e80c7920ff5ed746ca02e3d6dec483e831e
Matheus Balduino <matheus_mgb@hotmail.com>
2022-03-26 14:30:06 -0300

    controller working

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.example.algamoney.api.resource;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.example.algamoney.api.repository.CategoriaRepository;
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
+	@Autowired
 	private CategoriaRepository _repository;
 	
 	@GetMapping("/todos")
